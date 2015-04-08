559c97a4d722beb8d2773e2cb3cb02d87791d110
Vadim <detriment.ehl@gmail.com>
2015-04-08 05:01:41 -0400

    Made the first paddle moveable.

--- main.lua
+++ main.lua
@@ -1,12 +1,24 @@
 function love.load()
     paddle = love.graphics.newImage("paddle.png")
     ball = love.graphics.newImage("ball.png")
+    px1, px2, py1, py2 = 30, 750, 10, 10
+    bx = 380
+    by = 10
+end
+
+function love.update(dt)
+    if love.keyboard.isDown("down") then
+        py1 = py1 + 1
+    end
+    if love.keyboard.isUp("up") then
+        py1 = py1 - 1
+    end
 end
 
 function love.draw()
     --love.graphics.circle("fill", 300, 300, 10, 10)
-    love.graphics.draw(paddle, 30, 10)
-    love.graphics.draw(paddle, 750, 10)
+    love.graphics.draw(paddle, px1, py1)
+    love.graphics.draw(paddle, px2, py2)
     --love.graphics.draw(paddle)
     love.graphics.draw(ball, 380, 364)
 end
